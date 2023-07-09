201dcabcfcbaebd1c7d4700c62009865caee04af
Matthew Rao <mat_rao@hotmail.com>
2023-07-09 09:38:50 -0400

    THE API WORKS!!

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,24 +3,27 @@ require 'json'
 class WeatherController < ApplicationController
     def index
 
-        base_url = 'https://api.openweathermap.org/data/2.5/weather'
-        headers = {
-            lat: '45.5019',
-            lon: '73.5674',
-            date: '2023-07-04',
-            units: 'metric',
-            lang: 'en', # fr for french
-            appid: ENV['API_KEY']
-          }
+        lon = '45.4688'
+        lat = '73.8756'
+        date = '2023-07-04'
+        lang = 'en' # fr for french
+        units = 'metric'
+        appid = ENV['API_KEY']
+        
+        url = sprintf('https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s', lat, lon, appid)
       
-
-        response = HTTParty.get(base_url, headers: headers)
+        
+        response = HTTParty.get(url)
 
         if response.success?
-            data = response.parsed_response
-            # process data as needed
+            # data = response.parsed_response
+            weather_data = JSON.parse(response.body)
+
+            @temp = weather_data['main']['temp']
+            @desc = weather_data['weather'][0]['description']
         else
             error_message = response.parsed_response['error']
             # error handle
+        end
       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get '/weather', to 'weather#index'
+  root 'weather#index'
 
 end
