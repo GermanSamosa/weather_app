8517eba26ff32b2bb1d0f85be318f8ea1db15ae1
Matthew Rao <mat_rao@hotmail.com>
2023-08-17 09:45:51 -0400

    minor update

--- app/assets/javascripts/time.js
+++ app/assets/javascripts/time.js
@@ -19,41 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     setInterval(updateTime, 1000);
-    //new step
-
-   
-        // Get a reference to the HTML element where you want to display GPU data
-const gpuInfoElement = document.getElementById('gpu-info');
-
-// Fetch GPU data from the API
-fetch('/gpu_data')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Handle data and update the UI
-    const gpuInfo = data.gpu_info;
     
-    // Assuming the response format is plain text, you might need to parse it further
-    const gpuDataArray = gpuInfo.split('\n'); // Split by new lines
-
-    // Build HTML content to display the GPU data
-    let gpuDataHTML = '<ul>';
-    gpuDataArray.forEach(item => {
-      gpuDataHTML += `<li>${item}</li>`;
-    });
-    gpuDataHTML += '</ul>';
-
-    // Update the GPU data element with the generated HTML
-    gpuInfoElement.innerHTML = gpuDataHTML;
-  })
-  .catch(error => {
-    console.error('Error fetching GPU data:', error);
-    gpuInfoElement.innerHTML = 'Error fetching GPU data';
-  });
  
 
 });
